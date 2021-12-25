cfe86de9f5b3b2341b0c46738e24158560968a10
jstrivedi <96387345+jstrivedi@users.noreply.github.com>
2021-12-24 20:10:48 -0500

    Update L4-4-MissingInteger.py

--- L4-4-MissingInteger.py
+++ L4-4-MissingInteger.py
@@ -1,1 +1,38 @@
+# Task description
+# This is a demo task.
 
+# Write a function:
+
+# def solution(A)
+
+# that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+# For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+# Given A = [1, 2, 3], the function should return 4.
+
+# Given A = [−1, −3], the function should return 1.
+
+# Write an efficient algorithm for the following assumptions:
+
+# N is an integer within the range [1..100,000];
+# each element of array A is an integer within the range [−1,000,000..1,000,000].
+
+def solution(A):
+    # write your code in Python 3.6
+    # pass
+    sorA = sorted(A)
+    if sorA[-1]<1: return 1
+    if 1 not in sorA: return 1
+    else:
+        idx = sorA.index(1)
+        possorA = sorA[idx:]
+        num = 0
+        for i in range(1,possorA[-1]):
+            flag=0
+            while possorA[num]==i:
+                flag +=1
+                # possorA.remove(i)
+                num+=1
+            if flag==0: return i
+        return sorA[-1]+1
