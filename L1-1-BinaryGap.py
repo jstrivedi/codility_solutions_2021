bebcf5fdda82656de3b7721c8ed8c6bf36a4da26
jstrivedi <96387345+jstrivedi@users.noreply.github.com>
2021-12-24 19:40:44 -0500

    Update L1-1-BinaryGap.py

--- L1-1-BinaryGap.py
+++ L1-1-BinaryGap.py
@@ -1,1 +1,16 @@
+def solution(N):
+    # write your code in Python 3.6
+    # pass
+    binary = str(format(N,'b'))
+    
+    ones = []
+    for i in range(len(binary)):
+        if binary[i]=='1':
+            ones.append(i)
+    ans = 0
 
+    for i in range(len(ones)-1):
+        a = ones[i+1]-ones[i]-1
+        if a > ans:
+            ans=a
+    return ans
