52f9a107ef88cb98a98793a20dcac0c5e85d59cf
jstrivedi <96387345+jstrivedi@users.noreply.github.com>
2021-12-24 20:09:24 -0500

    Update L4-2-PermCheck.py

--- L4-2-PermCheck.py
+++ L4-2-PermCheck.py
@@ -1,1 +1,59 @@
 
+# Task description
+# A non-empty array A consisting of N integers is given.
+
+# A permutation is a sequence containing each element from 1 to N once, and only once.
+
+# For example, array A such that:
+
+#     A[0] = 4
+#     A[1] = 1
+#     A[2] = 3
+#     A[3] = 2
+# is a permutation, but array A such that:
+
+#     A[0] = 4
+#     A[1] = 1
+#     A[2] = 3
+# is not a permutation, because value 2 is missing.
+
+# The goal is to check whether array A is a permutation.
+
+# Write a function:
+
+# def solution(A)
+
+# that, given an array A, returns 1 if array A is a permutation and 0 if it is not.
+
+# For example, given array A such that:
+
+#     A[0] = 4
+#     A[1] = 1
+#     A[2] = 3
+#     A[3] = 2
+# the function should return 1.
+
+# Given array A such that:
+
+#     A[0] = 4
+#     A[1] = 1
+#     A[2] = 3
+# the function should return 0.
+
+# Write an efficient algorithm for the following assumptions:
+
+# N is an integer within the range [1..100,000];
+# each element of array A is an integer within the range [1..1,000,000,000].
+
+def solution(A):
+    # write your code in Python 3.6
+    # pass
+    n = len(A)
+    counter = [0]*n
+    for i in A:
+        if i>n: return 0
+        counter[i-1]+=1
+    if 0 not in counter:
+        return 1
+    else:
+        return 0
